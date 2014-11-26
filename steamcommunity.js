e99a120639cb1a7f01a28ddd71c148e45e2a1dc5
depsys <kdepsys@gmail.com>
2014-11-26 15:39:48 +0300

    d7

--- steamcommunity.js
+++ steamcommunity.js
@@ -288,8 +288,8 @@ function inventoryPageInit(){
 		var PopulateMarketActions_orig = window.PopulateMarketActions;
 		window.PopulateMarketActions = function (elActions, item) {
 			var res = PopulateMarketActions_orig.apply(this, arguments);
-			$('#iteminfo1_item_descriptors').append('<div class="descriptor"><br/>ClassID#: <span style="color:green">'+item.classid+'</span> Defindex#: <span style="color:red">'+item.app_data.def_index+'</span></div>');
-			$('#iteminfo0_item_descriptors').append('<div class="descriptor"><br/>ClassID#: <span style="color:green">'+item.classid+'</span> Defindex#: <span style="color:red">'+item.app_data.def_index+'</span></div>');
+			$('#item_desc_icon').before('<div class="descriptor"><br/>ClassID#: <span style="color:green">'+item.classid+'</span> Defindex#: <span style="color:red">'+item.app_data.def_index+'</span></div>');
+//			$('#iteminfo0_item_descriptors').append('<div class="descriptor"><br/>ClassID#: <span style="color:green">'+item.classid+'</span> Defindex#: <span style="color:red">'+item.app_data.def_index+'</span><br/></div>');
 			if (!item.marketable) {
 				return res;
 			}
