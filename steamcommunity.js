1f4671a5e937d7cf76e67d8dcd847a4342d88dea
depsys <kdepsys@gmail.com>
2015-01-26 21:05:03 +0300

    Update steamcommunity.js

--- steamcommunity.js
+++ steamcommunity.js
@@ -320,6 +320,9 @@ function inventoryPageInit(){
 			return res;
 		}
 	} else {
+		var PopulateMarketActions_orig = window.PopulateMarketActions;
+		window.PopulateMarketActions = function (elActions, item) {
+		var res = PopulateMarketActions_orig.apply(this, arguments);		
 		$('.descriptorDef').remove();
 		$('#iteminfo1_item_name').before('<div class="descriptorDef">ClassID#: <span style="color:green">'+item.classid+'</span>, Defindex#: <span style="color:red">'+item.app_data.def_index+'</span><br/></div>');
 		$('#iteminfo0_item_name').before('<div class="descriptorDef">ClassID#: <span style="color:green">'+item.classid+'</span>, Defindex#: <span style="color:red">'+item.app_data.def_index+'</span><br/></div>');		
